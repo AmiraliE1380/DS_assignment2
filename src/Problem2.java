cea6278765f32104669a92ca97a155c81f1dbb4f
admin <ebrahimzadeh.amirali@gmail.com>
2020-11-27 14:07:54 +0330

    Completed Problem2

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Stack;
 
 public class Problem2 {
@@ -11,35 +11,50 @@ public class Problem2 {
     }
 
     private void run() {
-        stack = new Stack<>();
+        stack = new Stack<String>();
         operators = new ArrayList<>();
         operators.add("+");
         operators.add("-");
-        operators.add("%");
+        operators.add("/");
         operators.add("*");
-        System.out.println(prefixToPostfix("+AB"));
+        System.out.println("Recursive function : " + prefixToPostfixRecursive("*+AB-CD"));
+        System.out.println("Non recursive function : " + prefixToPostfix("*+AB-CD"));
+        //Infix: (A+B)*(C-D)
     }
 
-    private String prefixToPostfix(String prefix) {
+    private String prefixToPostfixRecursive(String prefix) {
         if(prefix.length() == 0) {
             return print();
         }
 
-        if(operators.contains(prefix.indent(prefix.length()))) {
+        if(operators.contains(prefix.substring(prefix.length() - 1))) {
             String newString = stack.pop() + stack.pop() + prefix.substring(prefix.length() - 1);
             stack.push(newString);
         } else {
             stack.push(prefix.substring(prefix.length() - 1));
         }
-        return prefixToPostfix(prefix.substring(0, prefix.length() - 2));
+        return prefixToPostfixRecursive(prefix.substring(0, prefix.length() - 1));
+    }
+
+    private String prefixToPostfix(String prefix) {
+        int size = prefix.length();
+        for(int i = size - 1; i >=0; i--) {
+            if(operators.contains(String.valueOf(prefix.charAt(i)))) {
+                String newString = stack.pop() + stack.pop() + prefix.charAt(i);
+                stack.push(newString);
+            } else {
+                stack.push(String.valueOf(prefix.charAt(i)));
+            }
+        }
+        return print();
     }
 
     private String print() {
-        int size = stack.size();
-        String postfix = "";
-        for(int i = 0; i < size; i++) {
-            postfix.concat(stack.pop());
+        StringBuilder postfix = new StringBuilder();
+        Iterator iterator = stack.iterator();
+        while(iterator.hasNext()) {
+            postfix.append(stack.pop());
         }
-        return postfix;
+        return postfix.toString();
     }
 }
